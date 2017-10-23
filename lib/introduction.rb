f8b8dbd5e1a9cda21d1feaf0947c93512c8bc492
Yamuna Navada <yamunanavada@gmail.com>
2017-10-23 14:53:06 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ end
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I'm learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
